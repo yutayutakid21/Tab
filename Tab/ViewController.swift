c1495687cfbf0d80e7bda7ad49cc2c5b01e38be1
Yuta Fujii <yutayutakid21@gmail.com>
2020-01-21 17:04:07 +0900

    課題

--- Tab/ViewController.swift
+++ Tab/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /*
+     ・課題
+     Tabbarを継承し、NavigationControllerを継承した画面を2つ作成し、画面AにWebView(Google)を画面Bに、駅の名前のラベルがついたセルを3つ持ったTableView、セルをタップするとshowで画面遷移し、画面遷移先の背景色を上から、赤、青、黄色で実装してください。
+     
+     
+     */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
